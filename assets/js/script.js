b8d384940ae66987f0139b34011fb9f727fc12e5
= <devleonardo@hotmail.com>
2019-04-02 00:32:22 -0300

    Adicionado objetos

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,88 @@ function escrever(){
     document.write("<h3>Testando...</h3>");
 }
 
-var timer = setInterval(escrever, 1000);
+function iniciarTeste(){
+    var timer = setInterval(escrever, 1000);
+    var timer2 = setTimeout(escrever, 5000);
+}
 
-var timer2 = setTimeout(escrever, 5000);
+
+function carro(){
+    this.ano = "";
+    this.cor = "";
+    this.marca = "";
+    this.modelo = "";
+
+    this.inserirAno = function(ano){
+        this.ano = ano;
+    }
+
+    this.inserirCor = function(cor){
+        this.cor = cor;
+    }
+
+    this.inserirMarca = function(marca){
+        this.marca = marca;
+    }
+
+    this.inserirModelo = function(modelo){
+        this.modelo = modelo;
+    }
+}
+
+var newcar = new carro();
+
+function criarCarro(){
+
+    marca = prompt("Qual a marca do carro ?");
+    newcar.inserirMarca(marca);
+    document.getElementById('marca').innerHTML = newcar.marca;
+
+    modelo = prompt("Qual o modelo do carro ?");
+    newcar.inserirModelo(modelo);
+    document.getElementById('modelo').innerHTML = newcar.modelo;
+
+    ano = prompt("Qual o ano do carro ?");
+    newcar.inserirAno(ano);
+    document.getElementById('ano').innerHTML = newcar.ano;
+
+    cor = prompt("Qual a cor do carro ?");
+    newcar.inserirCor(cor);
+    document.getElementById('cor').innerHTML = newcar.cor;
+}
+
+
+
+function mudarMarca(){
+    marca = prompt("Qual a marca do carro ?");
+    newcar.inserirMarca(marca);
+    document.getElementById('marca').innerHTML = newcar.marca;
+}
+
+function mudarModelo(){
+    modelo = prompt("Qual o modelo do carro ?");
+    newcar.inserirModelo(modelo);
+    document.getElementById('modelo').innerHTML = newcar.modelo;
+}
+
+function mudarAno(){
+    ano = prompt("Qual o ano do carro ?");
+    newcar.inserirAno(ano);
+    document.getElementById('ano').innerHTML = newcar.ano;
+}
+
+function mudarCor(){
+    cor = prompt("Qual a cor do carro ?");
+    newcar.inserirCor(cor);
+    document.getElementById('cor').innerHTML = newcar.cor;
+}
+
+
+
+
+
+
+
+
+
 
